47b8c4d7a16bb888d1b6d72934b1a505fa44bb76
AmineBenAhmed <benahmed.amine.dev@gmail.com>
2020-09-11 12:05:46 +0100

    signout and auth middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password'
+import jwt from 'jsonwebtoken';
 
-const router = express.Router();
+const router = express.Router(); 
 
 router.post('/api/users/signin',
 [
@@ -14,14 +19,42 @@ router.post('/api/users/signin',
     .notEmpty()
     .withMessage('You must provide a password')
 ],
-(req: Request, res: Response) => {
-  const errors = validationResult(req);
-  console.log(errors);
+validateRequest,
+async (req: Request, res: Response) => {
+  //using the validateRequest middleware we can delete all the commented validators
+  // const errors = validationResult(req);
+  // console.log(errors);
   
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array());
+  // if (!errors.isEmpty()) {
+  //   throw new RequestValidationError(errors.array());
+  // } 
+
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (!existingUser) {
+    throw new BadRequestError('Invalid credentials');
   }
-  res.send('valid')
+
+  const passwordMatch = Password.compare(existingUser.password, password);
+
+  if (!passwordMatch) throw new BadRequestError('Invalid credentiels');
+
+   //Generate JWT
+   const userJwt = jwt.sign({
+    id: existingUser.id,
+    email: existingUser.email
+  }, process.env.JWT_KEY!);  //the process.env has two acceptable types either strin 
+                            //or undefined the exclamation mark says to typscript don't
+                            //worry we has verified this variable and it cannot be the both
+
+  //store it on session object
+  req.session = {
+    jwt: userJwt
+  };
+
+  res.status(201).send(existingUser);
+
 });
 
 export { router as signinRouter };
