e2bef9131685f25e8e2277bf0a5fb9a8d066ad4c
AmineBenAhmed <benahmed.amine.dev@gmail.com>
2020-10-31 21:52:43 +0100

    Updates

---
+++ common/src/middlewares/current-user.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface UserPayload {
+  id: string;
+  email: string;
+}
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
+export const currentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.session?.jwt) { //we added ? after session because session has two possible 
+                           //types string or undefined we use the ? to tell typeScript 
+                           //we has verified the req.session
+    return next();
+  }
+
+  try {
+    const payload = jwt.verify( //here we verifies if the token is valid or not
+      req.session.jwt,//to verify the token we must provide the token and the secret key
+      process.env.JWT_KEY!) as UserPayload;
+    req.currentUser = payload; //the verify method returns the creeds of the user specificly the used data to create the jwt
+  } catch (err) {
+
+  }
+  next();
+}
+
